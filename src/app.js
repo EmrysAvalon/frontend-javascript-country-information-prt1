6bf16a7d685e9be558b7f8de7d0a99bdfe528080
EmrysAvalon <tanja@hunen.nl>
2022-04-16 18:04:43 +0200

    uitwerkingen

--- src/app.js
+++ src/app.js
@@ -1,1 +1,57 @@
-console.log('Hallo daar!');
+import axios from "axios";
+
+const createElementPlus = (type, options = {}) => Object.assign(document.createElement(type), options);
+
+async function getCountryData() {
+    try {
+        const result = await axios.get('https://restcountries.com/v2/all');
+
+        const countryList = document.getElementById('countries');
+
+        const countriesSortedByPopulation = result.data.sort((a, b) => a.population - b.population);
+
+        const countries = countriesSortedByPopulation.map((country) => {
+
+            const countryContainer = createElementPlus('div', {
+            });
+
+            const countryName = createElementPlus('li', {
+                textContent: country.name,
+                className: setCountryColor(country.region)
+            });
+
+            const countryFlag = createElementPlus('img', {
+                src: country.flags.png,
+                width: 50,
+                height: 25
+            });
+
+            const countryPopulation = createElementPlus('li', {
+                textContent: `Has a population of ${country.population} people.`,
+                className: 'population'
+            });
+
+            countryContainer.append(countryFlag);
+            countryContainer.append(countryName);
+            countryList.append(countryContainer);
+            countryList.append(countryPopulation);
+            return country;
+        })
+
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+function setCountryColor(region) {
+
+    const posibleRegions = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania']
+
+    if (posibleRegions.includes(region)) {
+        return region.toString().toLowerCase();
+    } else {
+        return 'region-not-found';
+    }
+}
+
+getCountryData();
